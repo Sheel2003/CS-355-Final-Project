487d4f820b7329c4e5b3a014a0eb063d3e98dd51
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-04-30 21:57:35 -0400

    Implemented invincibility powerup

--- CS-355-Final-Projec.c
+++ CS-355-Final-Projec.c
@@ -7,10 +7,39 @@
 
 #define LENGTH 1000
 
-int snakeX[LENGTH], snakeY[LENGTH], snakeLength, trophy, trophyX, trophyY, MAXY, MAXX;
+typedef struct {
+    int x;
+    int y;
+    bool active;
+} PowerUp;
+
+int snakeX[LENGTH], snakeY[LENGTH], snakeLength, trophy, trophyX, trophyY, MAXY, MAXX, halfPerimeter, color;
 char direction;
 time_t trophyExpiration;
 bool paused = false;
+PowerUp powerUp;
+
+int isPowerUpOnSnake() {
+    for (int i = 0; i < snakeLength; i++) {
+        if (powerUp.x == snakeX[i] && powerUp.y == snakeY[i]) {
+            return 1; // PowerUp is on the snake
+        }
+    }
+    return 0; // PowerUp is not on the snake
+}
+
+void generatePowerUp() {
+    if (!powerUp.active && rand() % 100 == 0) {
+        do {
+            // Generate random position within screen bounds
+            powerUp.x = (rand() % (MAXY - 7)) + 6;
+            powerUp.y = (rand() % (MAXX - 3)) + 1;
+        } while (isPowerUpOnSnake() || powerUp.x == trophyX || powerUp.y == trophyY);
+
+        powerUp.active = true;
+        mvprintw(powerUp.x, powerUp.y, "+");
+    }
+}
 
 int isTrophyOnSnake() {
     for (int i = 0; i < snakeLength; i++) {
@@ -29,7 +58,7 @@ int generateTrophy() {
     do {
         trophyX = (rand() % (MAXY - 7)) + 6; // Generate random X position within screen bounds
         trophyY = (rand() % (MAXX - 3)) + 1; // Generate random Y position within screen bounds
-    } while (abs(trophyX - snakeX[0]) > maxDistance || abs(trophyY - snakeY[0]) > maxDistance || isTrophyOnSnake());
+    } while (abs(trophyX - snakeX[0]) > maxDistance || abs(trophyY - snakeY[0]) > maxDistance || isTrophyOnSnake() || (trophyX == powerUp.x && trophyY == powerUp.y));
 
     time_t currentTime;
     time(&currentTime);
@@ -62,9 +91,6 @@ void drawStatic() {
 
 void drawDynamic() {
 
-    // Draw the new trophy
-    mvprintw(trophyX, trophyY, "%d", trophy);
-
     // Draw the new head of the snake
     mvprintw(snakeX[0], snakeY[0], "0");
 
@@ -75,6 +101,17 @@ void drawDynamic() {
 
     // Draw the score
     mvprintw(1, 0, "Score: %d", snakeLength);
+
+    // Draw the new trophy
+    mvprintw(trophyX, trophyY, "%d", trophy);
+
+    // Draw the powerUp if active
+    if (powerUp.active) {
+        color++;
+        attron(COLOR_PAIR(color % 8));
+        mvprintw(powerUp.x, powerUp.y, "+");
+        attroff(COLOR_PAIR(color % 8));
+    }
 
     // Refresh the screen to apply changes
     refresh();
@@ -141,6 +178,81 @@ void moveSnake() {
     }
 }
 
+void invincibleDrawDynamic() {
+
+    // Draw the new trophy
+    mvprintw(trophyX, trophyY, "%d", trophy);
+
+    // Draw the new head of the snake
+    attron(COLOR_PAIR(color % 8));
+    mvprintw(snakeX[0], snakeY[0], "0");
+    attroff(COLOR_PAIR(color % 8));
+
+    // Draw the new body of the snake
+    for (int i = 1; i < snakeLength; i++) {
+        color++;
+        if (snakeLength % 8 != 0) {
+            attron(COLOR_PAIR(color % 8));
+            mvprintw(snakeX[i], snakeY[i], "o");
+            attroff(COLOR_PAIR(color % 8));
+        } else {
+            attron(COLOR_PAIR(color % 7));
+            mvprintw(snakeX[i], snakeY[i], "o");
+            attroff(COLOR_PAIR(color % 7));
+        }
+    }
+
+    // Draw the score
+    mvprintw(1, 0, "Score: %d", snakeLength);
+
+    // Refresh the screen to apply changes
+    refresh();
+}
+
+void invincibleCheckCollision() {
+    // Adjusted for borders
+    if (snakeX[0] == 5 || snakeX[0] == MAXY - 1 || snakeY[0] == 0 || snakeY[0] == MAXX - 2) {
+        gameOver(0);
+    }
+
+    // Checks if the snake collides with trophy
+    if (snakeX[0] == trophyX && snakeY[0] == trophyY) {
+        snakeLength += trophy;
+        trophy = generateTrophy();
+    }
+    // Checks if the trophy has expired
+    time_t currentTime;
+    time(&currentTime);
+    if (currentTime >= trophyExpiration) {
+        mvprintw(trophyX, trophyY, " ");
+        trophy = generateTrophy();
+    }
+
+}
+
+void invincibleInput() {
+    int ch = getch(); // Use int instead of char to handle arrow keys
+    switch(ch) {
+        case KEY_UP:
+            if (direction != 1) direction = 0; // Change direction to UP if not already moving DOWN
+            break;
+        case KEY_DOWN:
+            if (direction != 0) direction = 1; // Change direction to DOWN if not already moving UP
+            break;
+        case KEY_LEFT:
+            if (direction != 3) direction = 2; // Change direction to LEFT if not already moving RIGHT
+            break;
+        case KEY_RIGHT:
+            if (direction != 2) direction = 3; // Change direction to RIGHT if not already moving LEFT
+            break;
+        case 'p':
+            paused = !paused; // Toggle pause state
+            break;
+        default:
+            break;
+    }
+}
+
 void checkCollision() {
     // Adjusted for borders
     if (snakeX[0] == 5 || snakeX[0] == MAXY - 1 || snakeY[0] == 0 || snakeY[0] == MAXX - 2) {
@@ -164,6 +276,62 @@ void checkCollision() {
         mvprintw(trophyX, trophyY, " ");
         trophy = generateTrophy();
     }
+    // Checks if snake collides with powerup
+    if (snakeX[0] == powerUp.x && snakeY[0] == powerUp.y) {
+        time_t powerUpExpiration;
+        time(&powerUpExpiration);
+        powerUpExpiration += 20; // Power-up lasts for 20 seconds
+        
+        while(1) {
+        if (!paused) {
+            invincibleDrawDynamic(); // Draw dynamic elements (snake and trophies)
+            invincibleInput();
+            moveSnake();
+            generatePowerUp();
+            invincibleCheckCollision();
+            refresh();
+
+            int speed = halfPerimeter - snakeLength;
+            if (speed > 20)
+                usleep(speed * 1000); // Convert speed to microseconds
+            else
+                usleep(20 * 1000); // Convert 20 milliseconds to microseconds
+
+            if (snakeLength >= halfPerimeter){
+                endwin();
+                printf("You Win! Max Length Reached On Your Screen: %d\n", halfPerimeter);
+                exit(0);
+            }
+            time_t current;
+            time(&current);
+            if (current >= powerUpExpiration) {
+                powerUp.active = false;
+                break; // Exit the loop if power-up expires
+            }
+        }
+        else {
+            // Display a message indicating that the game is paused
+            int middleX = (MAXX - 12 - 5) / 2;
+            // Colors not working right //
+            //start_color();
+            //init_pair(1, COLOR_RED, COLOR_BLACK);
+            //attron(COLOR_PAIR(1));
+            mvprintw(5, (MAXX - 12 - 5) / 2, "Game Paused");
+            //attroff(COLOR_PAIR(1));
+            refresh();
+            while (1) {
+                if (getch() == 'p') {
+                    paused = false;
+                    //clear(); // Clear the screen before redrawing
+                    //drawStatic(); // Redraw static elements
+                    mvprintw(5, (MAXX - 12 - 5) / 2, "-----------");
+                    break;
+                }
+
+            }
+        }
+    }
+    }
 }
 
 int main() {
@@ -172,7 +340,7 @@ int main() {
     noecho();
     curs_set(0);
     keypad(stdscr, TRUE);
-    timeout(500); // Initial timeout value
+    timeout(1); // Initial timeout value
 
     signal(SIGINT, gameOver); // Signal handling for Ctrl + C
 
@@ -191,7 +359,7 @@ int main() {
     trophy = generateTrophy();
 
     // Shows score needed to win
-    int halfPerimeter = (MAXY - 1) + (MAXX - 1); // Perimeter of the border
+    halfPerimeter = (MAXY - 1) + (MAXX - 1); // Perimeter of the border
     printw("Winning score: %d", halfPerimeter);
     refresh();
     sleep(1);
@@ -199,20 +367,29 @@ int main() {
 
     drawStatic(); // Draw static elements at the start of the game
 
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE, COLOR_BLACK);
+    init_pair(5, COLOR_CYAN, COLOR_BLACK);
+    init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE, COLOR_BLACK);
+
     while(1) {
         if (!paused) {
             drawDynamic(); // Draw dynamic elements (snake and trophies)
             input();
             moveSnake();
+            generatePowerUp();
             checkCollision();
             refresh();
-            
-            // Adjust snake speed based on its length
+
             int speed = halfPerimeter - snakeLength;
             if (speed > 20)
-                timeout(speed);
+                usleep(speed * 1000); // Convert speed to microseconds
             else
-                timeout(20);
+                usleep(20 * 1000); // Convert 20 milliseconds to microseconds
 
             if (snakeLength >= halfPerimeter){
                 endwin();
